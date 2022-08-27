3344aa538d429596561d6a1ffddeae477f8ef19e
Wilsonborgia17 <netodrogo@gmail.com>
2022-08-26 22:58:33 -0500

    First Python´s Homework

---
+++ Work3.py
@@ -0,0 +1,11 @@
+print("El resultado de la suma 3 +3  es",3+3)
+print("El resultado de la resta 3-3 es", 3-3)
+print("El resultado de la multiplicación 3*3 es",3*3)
+print("El resultado de la división 3/3 es",3/3)
+print("El resultado de la potencia 3**3 es", 3**3)
+print("El resultado de la concatenacion  (´a´ + ´a´) es",("a"+"a"))
+print("El cociente parte entera de la expresión (3//3)es",(3//4))
+print("El modulo  de la expresión (3%5) es", (3%5))
+print("El resutado de la expresión (a*2)es", ("a"*2))
+print("El resutado de la expresión (´H´+´e´ + (2*´l´)+o+("")+ ´w´ + ´o´ + ´r´ + ´l´ + ´d´es",("H"+"e" + (2*"l")+("o") + (" ") + "w" + "o" + "r" + "l" + "d" ))
+
