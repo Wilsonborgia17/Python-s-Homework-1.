efd3c920407317bee53037d884cfc51ae5b320e3
Wilsonborgia17 <netodrogo@gmail.com>
2022-08-28 06:55:18 -0500

    Agregando titulo al trabajo

--- Work3.py
+++ Work3.py
@@ -1,3 +1,4 @@
+print("Comprenciòn de conceptos bàsicos de Python")
 print("Primer tarea de python en Git Hub para reforzar conocimientos de ambos cursos")
 print("El resultado de la suma 3 +3  es",3+3)
 print("El resultado de la resta 3-3 es", 3-3)
