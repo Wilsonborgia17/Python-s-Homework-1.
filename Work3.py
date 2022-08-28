d347ae40c90efcfa8f88e56b687804e5136c5b4f
Wilsonborgia17 <netodrogo@gmail.com>
2022-08-27 22:06:27 -0500

    Agregando motivos de publicaciòn

--- Work3.py
+++ Work3.py
@@ -1,3 +1,4 @@
+print("Primer tarea de python en Git Hub para reforzar conocimientos de ambos cursos")
 print("El resultado de la suma 3 +3  es",3+3)
 print("El resultado de la resta 3-3 es", 3-3)
 print("El resultado de la multiplicación 3*3 es",3*3)
