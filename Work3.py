d492d9960fbe004500ec616874e374ea2f9f4e09
Wilsonborgia17 <netodrogo@gmail.com>
2022-08-28 07:25:22 -0500

    agregue un parrafo problematico al final

--- Work3.py
+++ Work3.py
@@ -8,3 +8,5 @@ print("El cociente parte entera de la expresión (3//3)es",(3//4))
 print("El modulo  de la expresión (3%5) es", (3%5))
 print("El resutado de la expresión (a*2)es", ("a"*2))
 print("El resutado de la expresión (´H´+´e´ + (2*´l´)+o+("")+ ´w´ + ´o´ + ´r´ + ´l´ + ´d´es",("H"+"e" + (2*"l")+("o") + (" ") + "w" + "o" + "r" + "l" + "d" ))
+print("Este parrafo ocasionará un conflicto entre la rama master y rama2")
+
