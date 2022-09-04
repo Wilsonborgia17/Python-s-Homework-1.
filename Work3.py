9cfe5d187c6c0d12669ddaaae837c056364cf5c9
CengelBoles <ernesto.bermudez@alumnos.udg.mx>
2022-09-03 22:16:32 -0500

    Agregando descripciòn aqui en lugar de en README.md

--- Work3.py
+++ Work3.py
@@ -1,3 +1,23 @@
+"""Escribir un programa en Python que se llame “tarea3.py”, que presente en pantalla el resultado de
+cada una de las operaciones alfanuméricas y aritméticas disponibles en Python, de acuerdo a los
+siguientes requerimientos:
+1.- Se debe presentar cada una de las operaciones en una línea de código:
+Concatenación
+Repetición
+Suma
+Resta
+Multiplicación
+División
+Potencia
+Cociente (parte entera)
+Módulo (residuo)
+2.- Deberá incluir una línea para los operadores alfanuméricos y otra para los operadores
+aritméticos que calcule el resultado de una expresión que incluya por lo menos dos operadores en
+el caso de los alfanuméricos y cinco operadores en el caso de los aritméticos.
+3.- Deberá incluir, en cada operación, un mensaje que indique de qué operación se trata, tomar
+como base el archivo de ejemplo adjunto "ejemplo_para_tarea3.py" (este tiene un ejemplo de
+operadores alfanuméricos, dos ejemplos de operadores aritméticos y un ejemplo de expresiones
+más elaboradas)."""
 print("El resultado de la suma 5 +5  es",5+5)
 print("El resultado de la resta 3-3 es", 3-3)
 print("El resultado de la multiplicación 3*3 es",3*3)
