b52f403c307393f9ca3c89aacc49a82ef97f2e17
CengelBoles <ernesto.bermudez@alumnos.udg.mx>
2022-09-05 06:26:57 -0500

    Agragando nueva tarea

---
+++ Work4.py
@@ -0,0 +1,97 @@
+"""Escribir un programa que:
+1.- Cree dos variables, una de nombre "espacios" y otra de nombre
+"asteriscos" y les asigne los valores numéricos 25 y 1
+respectivamente.
+2- Presente en pantalla el resultado de una expresión alfanumérica
+que sea la concatenación de: ' ' (un espacio) repetido tantas veces
+como sea el valor de la variable "espacio" concatenado con '*' (un
+asterisco) repetido tantas veces como sea el valor de la variable
+"asteriscos"
+3.- Decremente la variable "espacios" en uno (1) e incremente la
+variable "asteriscos" en dos (2), esto deberá hacerse mediante las
+expresiones aritméticas respectivas.
+4.- Presente en pantalla el resultado de una expresión alfanumérica
+que sea la concatenación de: ' ' (un espacio) repetido tantas veces
+como sea el valor de la variable "espacio" concatenado con '*' (un
+asterisco) repetido tantas veces como sea el valor de la variable
+"asteriscos"
+5.- Repita los puntos 3 y 4 hasta que la variable “espacios” llegue a
+cero (0)"""
+espacios = 25
+asteriscos = 1
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
+espacios = espacios - 1
+asteriscos = asteriscos + 2
+print(" "*espacios + "*"*asteriscos)
